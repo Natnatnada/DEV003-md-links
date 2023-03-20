ae94ea032e2e6fd7e6379fae3b5cb3cb236e6fa6
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-03-20 18:25:04 -0300

    :white_check_mark: :sparkles:se agregan test y nuevas funciones en functionsApi.js

--- functionsApi.js
+++ functionsApi.js
@@ -4,24 +4,40 @@ const fs = require('fs')
 //se importa node path module
 const path = require('path')
 
-//para saber si una ruta existe fs.existsSync(path) retorna un booleano  // fs.exists deprecated
+//para saber si una ruta existe fs.existsSync(path) retorna un booleano test ok // fs.exists deprecated
 function isValidPath(docpath) {
     return fs.existsSync(docpath);
 }
-//para saber si es una ruta absoluta path.isAbsolute(path) retorna un booleano
-function isAbsolutePath(docpath){
+//console.log(isValidPath('README.md'))
+
+//para saber si es una ruta absoluta path.isAbsolute(path) retorna un booleano, test ok
+function isAbsolutePath(docpath) {
     return path.isAbsolute(docpath)
 
 }
+// console.log(isAbsolutePath('/archivoprueba.md'));
 
-// //convertir la ruta a  
-// w3school relative()	Returns the relative path from one specified path to another specified path
-// w3school resolve()	Resolves the specified paths into an absolute path
+// convertir la ruta relativa a absoluta path.resolve? 
+function convertToAbsolute(docpath) {
+    return path.resolve(docpath)
+}
+//console.log(convertToAbsolute('./pruebauno/archivoprueba.md'));
+
 //saber si es un directorio .isDirectory
+
 //obtiene la extension del archivo para saber si es md path.extname(path)
+
+//leer los archivos !!!!!
+
 //si es directorio debe recorrer los archivos
+
+//contiene archivos md
+
+//extraer los archivos
+
 module.exports = {
     isValidPath,
-    isAbsolutePath
+    isAbsolutePath,
+    convertToAbsolute
     //aqui se indican las rutas que se estan exportando para luego usar en mdlinks
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,15 +1,51 @@
 //importe desde index
 const { mdLinks } = require('../index.js')
-
-describe('mdLinks', () => {
-
-  it('debería ser una función', () => {
-    expect(typeof mdLinks).toBe('function');
+const { isValidPath,
+  isAbsolutePath,
+  convertToAbsolute
+} = require('../functionsApi')
+//revisar test proyecto2
+//deberia retornar false si la ruta no existe,  este caso
+describe('isValidPath', () => {
+  it('Deberia retornar false .', () => {
+    //ruta falsa
+    const path = 'READMEUNO.md'
+    expect(isValidPath(path)).toBe(false)
   });
-  it('Debe rechazar si el path no existe', () => {
-    return mdLinks('/lab/proyecto/noexiste.md').catch((error) => {
-      expect(error).toBe('La ruta no existe')
-    });
+  it('Deberia retornar true .', () => {
+    //ruta verdadera
+    const path = 'README.md'
+    expect(isValidPath(path)).toBe(true)
   });
-
 });
+
+describe('isAbsolutePath', () => {
+  it('Deberia retornar isAbsolutePath true.', () => {
+    const path = '/archivoprueba.md'
+    expect(isAbsolutePath(path)).toBe(true)
+  });
+  it('Deberia retornar isAbsolutePath false.', () => {
+    const path = 'archivoprueba.md'
+    expect(isAbsolutePath(path)).toBe(false)
+  });
+});
+describe('convertToAbsolute', () => {
+  it('Deberia retornar la ruta absoluta.', () => {
+    const path = './pruebauno/archivoprueba.md'
+    //dobre backslash para que tome la ruta
+    expect(convertToAbsolute(path)).toBe('C:\\Laboratoria Proyectos\\DEV003-md-links\\pruebauno\\archivoprueba.md')
+  })
+});
+
+// describe('mdLinks', () => {
+
+//   it('debería ser una función', () => {
+//     expect(typeof mdLinks).toBe('function');
+//   });
+//   it('Debe rechazar si el path no existe', () => {
+//     return mdLinks('/lab/proyecto/noexiste.md').catch((error) => {
+//       expect(error).toBe('La ruta no existe')
+//     });
+//   });
+
+// });
